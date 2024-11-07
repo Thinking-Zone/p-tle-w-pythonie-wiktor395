a2b259935ac2b80809aa669c6e7ceb65e801ea02
wiktor395 <lolscfy@gmail.com>
2024-11-07 10:00:44 +0100

    Update zadanie6.py

---
+++ zadanie6.py
@@ -0,0 +1,13 @@
+# Pętla `for` jest najczęściej używana, gdy wiemy, ile razy chcemy wykonać daną operację.
+# Jest to idealne rozwiązanie, gdy mamy z góry określoną liczbę iteracji lub chcemy iterować
+# po jakimś zakresie (np. lista, zbiór, ciąg liczb, itp.) i automatycznie zarządzać indeksami.
+
+# Pętla `while` jest bardziej elastyczna i używana, gdy nie wiemy dokładnie, ile razy
+# pętla ma być wykonana. Pętla `while` działa tak długo, jak warunek pozostaje prawdziwy.
+# Zatem, w pętli `while` warunek decyduje o kontynuowaniu iteracji, a pętla może trwać
+# nieokreśloną liczbę razy, jeśli warunek jest zawsze spełniony lub nigdy nie zostanie spełniony.
+
+# Podsumowanie:
+# - Pętla `for` jest stosowana, gdy mamy określoną liczbę iteracji lub gdy iterujemy po zbiorze danych.
+# - Pętla `while` jest bardziej uniwersalna i stosowana, gdy nie wiemy, kiedy pętla ma się zakończyć, 
+#   zależy to od zmieniającego się warunku.
